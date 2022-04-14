8e04cbe046ed1b867a8767078743098c22215d09
Gareth Breeze <gareth.breeze@hypaship.com>
2022-04-14 10:33:57 +0100

    Add task model declaration and alembic script to create task table

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey, Date, DateTime
+from sqlalchemy.orm import relationship
 from sqlalchemy.sql.expression import text
 from sqlalchemy.sql.sqltypes import TIMESTAMP
 
@@ -12,3 +13,18 @@ class User(Base):
     email = Column(String, nullable=False, unique=True)
     password = Column(String, nullable=False)
     created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
+
+
+class Task(Base):
+    __tablename__ = 'tasks'
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    title = Column(String, nullable=False)
+    description = Column(String, nullable=False)
+    deadline = Column(Date())
+    completed = Column(Boolean(create_constraint=False), default=False, nullable=False)
+    owner_id = Column(Integer, ForeignKey('users.id', name='tasks_owner_id_fkey', ondelete='CASCADE'), nullable=False)
+    creation_date = Column(DateTime(), server_default=text('now()'))
+    last_update = Column(DateTime(), server_default=text('now()'), onupdate=text('now()'))
+
+    owner = relationship('User')
