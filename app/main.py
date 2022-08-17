befa59565f4e4b5b657f54e42d1b291e856d70fb
Gareth Breeze <gareth.breeze@hypaship.com>
2022-08-17 13:29:26 +0100

    Add CORSmiddleware to allow all domains at the moment so can test sending data from frontend

--- app/main.py
+++ app/main.py
@@ -1,10 +1,23 @@
 from fastapi import FastAPI, Depends
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi_pagination import add_pagination
 
 from app.routers import users, auth, tasks
 from app import oauth2, models
 
 app = FastAPI()
+# command uvicorn app.main:app to run server
+
+# At the moment allow all domains and all headers to connect to this API maybe change when push to deployment etc.
+origins = ["*"]  # This means all domains can access our API
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(users.router)
 app.include_router(auth.router)
